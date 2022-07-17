0fe8e14d51e475f16b6f80c39091fc553484cf00
Monika <monidavies@gmail.com>
2022-07-16 23:39:30 -0700

    Basic app framework

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React from 'react';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Main from './Main';
 
-function App() {
+class App extends React.Component() {
+  render() {
   return (
     <div className="App">
-      
+      <Header title={"Gallery of Horns"} />
+      <Main/>
+      <Footer author={"Page by Monika Davies"} />
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import HornedBeast from './HornedBeast';
+
+class Main extends React.Component {
+    render() {
+        return (
+            <>
+            <HornedBeast title={""} image={""} description={""} />
+            <HornedBeast title={""} image={""} description={""} />
+            </>
+        )
+    }
+}
+
+export default Main;
