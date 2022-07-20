7258b6208e58b9f6fcb8d7153dde3507b938b44a
Monika <monidavies@gmail.com>
2022-07-20 14:10:25 -0700

    Add feature 2, a favorites counter

--- README.md
+++ README.md
@@ -21,3 +21,18 @@ Times: 8:50pm - 9:20pm (got map to work with JSON file and all content loads exc
 8:00am - 9:00am (finally got images to load)
 
 Actual time: 1.5 hours
+
+
+Feature #2 Allow users to vote for their favorite beast
+
+Estimate time needed to complete: 40 minutes
+
+Start Time: 1:16 PM
+
+Finish Time: 2:06 PM
+Note: Got stuck on "this" binding - first used an arrow function, click resulted in NAN output. I tried to use a regular
+function declaration instead (since these don't have "this" binding issues of arrow functions) but then the output wouldn't change at all, couldn't even print a value I passed in to set state (not sure why this function declaration didn't work).
+Switched back to arrow functions and tried a few different ways to bind "this" and finally found a way to get the right 
+behavior, but not sure if this is the optimal solution.
+
+Actual time: 50 minutes
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,12 +1,32 @@
 import React from "react";
 
 class HornedBeast extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            favorites: 0
+        }
+        this.incrementFav = this.incrementFav.bind(this);
+    };
+
+
+    incrementFav = () => {
+        this.setState((prevState) => ({
+            favorites: prevState.favorites + 1
+        }));
+    }
+
     render() {
         return (
             <>
                 <h2>{this.props.title}</h2>
-                <img src={this.props.image_url} alt={this.props.description} height={400}/>
+                <img 
+                    src={this.props.image_url} 
+                    alt={this.props.description} 
+                    height={400}
+                    onClick={this.incrementFav}/>
                 <p>{this.props.description}</p>
+                <h3>💕: {this.state.favorites}</h3>
             </>
         )
     }
