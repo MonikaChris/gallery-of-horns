2615ec33d1149fd396d37762073a0e0e02158e21
Monika <monidavies@gmail.com>
2022-07-24 09:11:06 -0700

    Make modals display title, image, description

--- src/SelectedBeast.js
+++ src/SelectedBeast.js
@@ -4,12 +4,14 @@ import Button from 'react-bootstrap/Button';
 
 class SelectedBeast extends React.Component {
     render() {
+        console.log(this.props);
         return (
             <Modal show={this.props.showModal} onHide={this.props.closeModal}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
+                    <Modal.Title>{this.props.beast.title}</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <img src={this.props.beast.image_url} alt={this.props.beast.title} /> 
+                <Modal.Body>{this.props.beast.description}</Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={this.props.closeModal}>
                         Close
