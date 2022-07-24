c69c8c2aad9035b4047c07eab123f672ccc4dcbf
Monika <monidavies@gmail.com>
2022-07-23 17:30:05 -0700

    Refactor to store data in App and pass to Main

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import Header from './Header';
 import Footer from './Footer';
 import Main from './Main';
 import Container from 'react-bootstrap/Container';
+import data from './data.json';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {beastData : data}
+  }
+  
   render() {
     return (
       <Container className="App">
         <Header title={"Gallery of Horns"} />
         <Container id="main">
-          <Main />
+          <Main beastData={this.state.beastData}/>
         </Container>
         <Footer author={"Page by Monika Davies"} />
       </Container>
--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,17 @@
 import React from "react";
 import HornedBeast from './HornedBeast';
-import data from './data.json';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 
 
 class Main extends React.Component {
+      
     render() {
         return (
             <Container>
                 <Row xs={1} md={2} lg={3} className="g-4">
-                    {data.map(beast =>
+                    {this.props.beastData.map(beast =>
                         <Col>
                             <HornedBeast
                                 title={beast.title}
